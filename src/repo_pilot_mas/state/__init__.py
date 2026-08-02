053088cd1105db3677b61d9928d44a14accaa1cc
Yan Jiahang <yyyyjh96@gmail.com>
2026-08-03 01:10:27 +0800

    feat: export hypothesis resolution state

--- src/repo_pilot_mas/state/__init__.py
+++ src/repo_pilot_mas/state/__init__.py
@@ -2,5 +2,16 @@
 
 from repo_pilot_mas.state.blackboard import ArtifactStore, Blackboard
 from repo_pilot_mas.state.checkpoint import CheckpointBundle, CheckpointStore
+from repo_pilot_mas.schemas.hypothesis_resolution import (
+    HypothesisResolution,
+    HypothesisResolutionStatus,
+)
 
-__all__ = ["ArtifactStore", "Blackboard", "CheckpointBundle", "CheckpointStore"]
+__all__ = [
+    "ArtifactStore",
+    "Blackboard",
+    "CheckpointBundle",
+    "CheckpointStore",
+    "HypothesisResolution",
+    "HypothesisResolutionStatus",
+]
